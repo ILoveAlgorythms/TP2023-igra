2b459d250d957ecbac27ddea166dda703131c392
Ольга Касапенко <kasapenko.oa@phystech.edu>
2023-03-17 19:49:47 +0300

    hehhe

--- animal.cpp
+++ animal.cpp
@@ -1,1 +1,20 @@
 #include "animal.hpp"
+#include "resources.hpp"
+
+void Animal::Feed() {
+  if (bellyful_ <= 2) {
+    bellyful_ += 2;
+  } else {
+    bellyful_ = 5;
+  }
+  happiness_level_ = 5;
+}
+
+void Animal::Sell() { // только снимаем с вектора животного, а функция получения денег будет связана с этой за счет паттерна composite
+ //снять с вектора
+ //получить деньги
+}
+
+void Animal::GiveResources() {
+  
+}
