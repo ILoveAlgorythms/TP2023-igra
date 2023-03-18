24f6ff07f575a9c35a3e7b0e949952b3f29b3913
somehuman <->
2023-03-18 20:33:26 +0300

    Started SFML, started working on build

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include "animal.hpp"
-#include "ferm.hpp"
+#include "farm.hpp"
 #include "player.hpp"
-
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 int main() {
+  sf::RenderWindow window(sf::VideoMode(800, 450), "SAD FARM");
+  //sounds:
+  sf::SoundBuffer soundBuffer;
+  if (!soundBuffer.loadFromFile("543.mp3")) {
+    throw "no file";
+  }
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+      if (event.type == sf::Event::MouseButtonPressed) {
+        sf::Sound sound;
+        sound.setBuffer(soundBuffer);
+        sound.setPitch(1.2);
+        sound.play();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
   return 0;
 }
+
