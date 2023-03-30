5cc681c4f17044af64582fae2cd1152164c69432
Ольга Касапенко <kasapenko.oa@phystech.edu>
2023-03-30 12:59:02 +0300

    saving_sys2

--- saving_system.cpp
+++ saving_system.cpp
@@ -1,22 +1,19 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <unordered_map>
 #include "farm.hpp"
-std::unordered_map<std::string, Animal*>Pets_; 
-std::unordered_map<std::string, TypeOfBuildings>Buildings_;
 
-void Save_data() {
+void Save_data(Base& base) {
   std::ofstream out;
   out.open("data.txt");
   if (out.is_open()) {
     out << "Animals:" << std::endl;
-    for (auto it = Pets_.begin(); it != Pets_.end(); it++) {
+    for (auto it = base.Pets_.begin(); it != base.Pets_.end(); it++) {
       out << (it->first) << " "; 
       out << (it->second) << std::endl;
     }
     out << "Buildings:" << std::endl;
-    for (auto it = Buildings_.begin(); it != Buildings_.end(); it++) {
+    for (auto it = base.Buildings_.begin(); it != base.Buildings_.end(); it++) {
       out << (it->first) << " "; 
       out << (it->second) << std::endl;
     }
